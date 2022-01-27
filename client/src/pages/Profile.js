c8074d463fc47aeb51f7bf58695592b6bd48b37d
mtlankenau <mtlankenau@gmail.com>
2022-01-27 15:37:33 -0500

    added addFriend mutation functionality. accomplished through 'Add friend' button that conditionally renders on other user's profile pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Redirect, useParams } from 'react-router-dom';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -32,6 +35,16 @@ const Profile = () => {
     return <div>Loading...</div>
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
 
   return (
     <div>
@@ -39,6 +52,11 @@ const Profile = () => {
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
