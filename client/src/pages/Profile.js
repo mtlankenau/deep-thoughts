7a4970d3c79ee6565ecd606defec9a28090642bf
mtlankenau <mtlankenau@gmail.com>
2022-01-27 16:28:58 -0500

    created ThoughtForm component. renders on Profile & Home pages (not on your own profile page though). use useState hooks for thoughttext & character count

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 import Auth from '../utils/auth';
 
 const Profile = () => {
@@ -71,6 +72,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
